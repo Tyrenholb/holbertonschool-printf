a8688858d3dedbfe88b9318f486afab0be5ab3a5
Tyren Howarth <14807@holbertonstudents.com>
2026-08-05 09:26:08 +1000

    Updated printf

--- _printf.c
+++ _printf.c
@@ -1,8 +1,31 @@
-#include <limits.h>
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 
-Int _printf( )
+
+int _printf(const char *format, ...)
 {
+	int i = 0;
+	int count = 0;
 
+	if (format == NULL)
+		return (-1);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] == '%')
+		{
+			write(1, "%", 1);
+			i += 2;
+			count++;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			i++;
+			count++;
+		}
+	}
+
+	return (count);
 }
