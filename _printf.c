be9cfbd38a151bc53f89320fa5a51bcbe62c43d8
14822-Ab-L <14822holbertonstudents.com>
2026-08-11 12:01:10 +1000

    Add %i handle specifier

--- _printf.c
+++ _printf.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * _printf - Prints normal text, characters, strings, and percent signs.
+ * @format: The format string.
+ *
+ * Return: Number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
-	int i;
-	int count;
-	char c;
 	va_list args;
+	char *str;
+	char c;
+	int i;
+	int j;
+	int count;
+
+	int number;
+	int div;
+	int digit;
 
 	i = 0;
 	count = 0;
@@ -17,33 +28,118 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 
 	while (format[i] != '\0')
-	{   /* Handle % */
+	{
+		/* Handles % at the end of the string */
+		/* %c, %d etc - if there is no c after the % it return -1 because there's a incomplete specifier */
+		if (format[i] == '%' && format[i + 1] == '\0')
+		{
+			va_end(args);
+			return (-1);
+		}
+		/* Handle %% Both Tyren & Albert */
 		if (format[i] == '%' && format[i + 1] == '%')
 		{
 			write(1, "%", 1);
-			i ++;
-			count++;
-		}
-
-		/* Print a character */
-        else if (format[i] == '%' && format[i + 1] == 'c')
-        {
-			c = va_arg(args, int);
-            write(1, &c, 1); 
 			count++;
 			i++;
-        }
+		}
+		
+		/* Handle %c - Alberts code*/
+		else if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			c = (char)va_arg(args, int);
+			write(1, &c, 1);
+			count++;
+			i++;
+		}
+		/* Handle %s - Tyrens code*/
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+			str = va_arg(args, char *);
 
-        /*This handle normal characters*/
+			if (str == NULL)
+				str = "(null)";
+
+			j = 0;
+
+			while (str[j] != '\0')
+			{
+				write(1, &str[j], 1);
+				count++;
+				j++;
+			}
+
+			i++;
+		}
+		/* Handle %d - Alberts code*/
+		else if (format[i] == '%' && format[i + 1] == 'd')
+		{
+			number = va_arg(args, int);
+
+			/*
+			If the number is negative, print its minus sign, count (count++) that character, 
+			then turn the number positive. The "-" has already been sent to the screen. 
+			It doesn't get attached to the number later. It is simply sitting there as the first character that was printed
+			The "-" is the very first character since we explicitly send to write().
+			*/
+			if (number < 0)
+			{
+				write(1, "-", 1);
+				count++;
+				number = number * -1;
+			/*
+			How the loop works for negative value:
+			1. check value if it's negative:
+				-762534
+			2. If yes then Print: 
+				"-"
+			3. Change value to negative value to postive:
+				-762534 -> 762534
+			4. The rest of our digit-printing code can handle the digits normally - Print digits:
+				7 -> 6 -> 2 -> 5 -> 3 -> 4 
+			5. Final output:
+				-762534
+			*/
+
+			}
+
+			if (number == 0)
+			{
+				write(1, "0", 1);
+				count++;
+			} 
+			else 
+			{
+				
+				/*Find the first digit*/
+				div =  1;
+
+				while(number / div >= 10) /*Keep looping while number / div is 10 or greater*/
+
+					div = div * 10;
+					
+				/* Print each digit */				
+				while (div > 0)
+				{
+					digit = ((number / div) % 10) + '0'; // Reduce n to the remaining digits
+
+					write(1, &digit, 1); /* */
+					count++; /**/
+					div = div / 10; // Move the divisor one decimal place to the right
+					
+				}
+			}
+			i++;
+		}
+
+		/* Handle normal text - Both Tyren & Albert */
 		else
 		{
 			write(1, &format[i], 1);
 			count++;
-			
 		}
 
-	i++;
-
+		i++;
 	}
 
 	va_end(args);
--- main.c
+++ main.c
@@ -12,13 +12,11 @@ int main(void)
     int len;
     int len2;
     unsigned int ui;
-    /*void *addr;*/
-
+    /* void *addr; */
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-   /* addr = (void *)0x7ffe637541f0; */
-
+    /* addr = (void *)0x7ffe637541f0; */
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -33,18 +31,28 @@ int main(void)
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    return (0);
+    _printf("Progress: 100%% complete\n\n");
+
+    printf("\n\n\n\n");
+    /* test mains for now*/
+   
+    printf("[%d]\n", len);
+    _printf("[%d]\n", len); 
+    printf("[%d]\n", -45213213);
+    _printf("[%d]\n", -45213213); 
+
+     printf("[%d]\n", 0);
+    _printf("[%d]\n", 0); 
+
+     _printf("Postuve:[%d]\n", 762534);
+    printf("Postuve:[%d]\n", 762534);
+
+    return(0);
 }
 
-/*
-
- %u -  
- %d - 
- %i - 
- %s - 
- %c - 
- %x - Hexadecimal 
-
- %p
-
-*/
+/* %u is the format specifier for an unsigned decimal integer - postive or zero ints only. */
+/* %d is a format specifier used as a placeholder for a signed decimal integer. */
+/* %i = for specifiier used to represent a single integer */
+/* %s is a format specifier used to represent text strings */
+/* %x is a format specifier used to print or read unsigned integers in hexadecimal (base-16) format using lowercase letters */
+/* %p format specifier is used within input/output functions like printf() and scanf() to print or read raw memory addresses */
